c37c28c7cbf2cc9664b0d7748143a89d4cf4ea5e
Sergey-Yachmennikov <otsmmm2013@gmail.com>
2026-09-07 09:30:28 +0300

    initial commit

---
+++ src/main/java/com/erevan/async_task_handler/AsyncTaskHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.erevan.async_task_handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncTaskHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncTaskHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erevan/async_task_handler/AsyncTaskHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erevan.async_task_handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncTaskHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
